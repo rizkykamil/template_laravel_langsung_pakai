e4044545df24c6e0068304cfcd38967f4f007962
rabbaniikamil <rabbaniikamil@gmail.com>
2024-10-07 08:13:34 +0000

    menambahkan push and stack di blade

--- resources/views/layouts/script.blade.php
+++ resources/views/layouts/script.blade.php
@@ -2,3 +2,4 @@
 <script src="{{asset('assets/libs/simplebar/simplebar.min.js')}}"></script>
 <script src="{{asset('assets/libs/feather-icons/feather.min.js')}}"></script>
 <script src="{{asset('assets/js/app.js')}}"></script>
+@stack('custom_js');
